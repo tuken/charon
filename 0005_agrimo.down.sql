32744f7faa82d72d51272253b892eb682a061f60
tuken <tuken@gmail.com>
2026-03-05 20:54:44 +0900

    update: 0005
    weather_codes.codeをsmallintに
    weathers → weather_codes

--- 0005_agrimo.down.sql
+++ 0005_agrimo.down.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `work_reports` DROP FOREIGN KEY `fk_work_reports_weather`;
 
 ALTER TABLE `work_reports` DROP INDEX `idx_work_reports_weather_code`;
+
+ALTER TABLE `weather_codes` MODIFY COLUMN `code` int unsigned NOT NULL COMMENT '天気コード';
