d13e3a855a478e8786785bf05ef7d2ef9e5104e1
tuken <tuken@gmail.com>
2026-03-05 21:00:42 +0900

    update: 0005
    `weathers` TO `weather_codes`のリネーム処理を追加

--- 0005_agrimo.down.sql
+++ 0005_agrimo.down.sql
@@ -2,4 +2,6 @@ ALTER TABLE `work_reports` DROP FOREIGN KEY `fk_work_reports_weather`;
 
 ALTER TABLE `work_reports` DROP INDEX `idx_work_reports_weather_code`;
 
+RENAME TABLE `weathers` TO `weather_codes`;
+
 ALTER TABLE `weather_codes` MODIFY COLUMN `code` int unsigned NOT NULL COMMENT '天気コード';
--- 0005_agrimo.up.sql
+++ 0005_agrimo.up.sql
@@ -1,5 +1,7 @@
 ALTER TABLE `weather_codes` MODIFY COLUMN `code` smallint unsigned NOT NULL COMMENT '天気コード';
 
+RENAME TABLE `weather_codes` TO `weathers`;
+
 ALTER TABLE `work_reports` ADD INDEX `idx_work_reports_weather_code` (`weather_code`);
 
-ALTER TABLE `work_reports` ADD CONSTRAINT `fk_work_reports_weather` FOREIGN KEY (`weather_code`) REFERENCES `weather_codes` (`code`);
+ALTER TABLE `work_reports` ADD CONSTRAINT `fk_work_reports_weather` FOREIGN KEY (`weather_code`) REFERENCES `weathers` (`code`);
